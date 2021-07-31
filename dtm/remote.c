5457cafbcb66a68c449bc32e37179a916490c923
Nikita Danilov <nikita.danilov@seagate.com>
2021-07-31 00:02:18 -0400

    Uncommment unused function.

--- dtm/remote.c
+++ dtm/remote.c
@@ -245,9 +245,8 @@ static int rem_rpc_deliver(struct m0_rpc_machine *mach,
 	return 0;
 }
 
-/* unused
 static void rem_rpc_redo_replied(struct m0_rpc_item *item)
-{} */
+{}
 
 static const struct m0_fop_type_ops rem_rpc_ftype_ops = {
 	/* nothing */
@@ -261,10 +260,9 @@ static const struct m0_rpc_item_ops rem_rpc_item_sender_ops = {
 	/* nothing */
 };
 
-/* unused
 static const struct m0_rpc_item_ops rem_rpc_item_redo_ops = {
 	.rio_replied = &rem_rpc_redo_replied
-}; */
+};
 
 M0_INTERNAL void m0_dtm_local_remote_init(struct m0_dtm_local_remote *lre,
 					  struct m0_uint128 *id,
