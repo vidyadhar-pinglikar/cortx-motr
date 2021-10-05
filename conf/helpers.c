c3312c05e7b4a71c0e3b2557d92d4af3994c5dab
Upendra Patwardhan <upendra.patwardhan@seagate.com>
2021-10-05 12:59:17 +0530

    Fix build error.
    Signed-off-by: Upendra Patwardhan <upendra.patwardhan@seagate.com>

--- conf/helpers.c
+++ conf/helpers.c
@@ -629,8 +629,8 @@ M0_INTERNAL int m0_conf_confc_ha_update_async(struct m0_confc *confc,
 	int32_t               total;
 	int                   rc;
 	int                   i = 0;
-	m0time_t              start = m0_time_now();
-	m0time_t              end;
+	m0_time_t              start = m0_time_now();
+	m0_time_t              end;
 
 	total = m0_tl_reduce(m0_conf_cache, obj, &cache->ca_registry, 0,
 	                     + (m0_fid_tget(&obj->co_id) ==
